84324dd9746613d3abd592b9395fe111eb6668aa
Afdiko Mayte <afdikomayte@gmail.com>
2023-09-09 21:11:40 +0700

    init mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/afdikomayte/learn-go-web
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "it this root page")
+	})
+
+	fmt.Println("server run at http://localhost:8080")
+
+	//server
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
